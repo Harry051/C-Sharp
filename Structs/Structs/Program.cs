7f9cca0c555895f1b5c5e7432e6ca7e600b534c6
Harry <109173178+Harry051@users.noreply.github.com>
2023-04-02 20:32:03 +0100

    Add files via upload

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Structs
+{
+
+        // Although very similar to a Class. A Struct must return a value
+        // Also they can not implement Constructors
+        // They do NOT support Inheritance
+        // Can't have a null reference      All values MUST be assigned.
+    struct Game
+    {
+ 
+        public string name;
+        public string developer;
+        public double rating;
+        public string releaseDate;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"The first game is called {name}, developed by {developer}.\nIt scored a rating of {rating}.\tReleased on {releaseDate}.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game fifa;
+
+            fifa.name = "FIFA 2023";
+            fifa.developer = "EA Sports";
+            fifa.rating = 4.2;
+            fifa.releaseDate = "3rd March 2023";
+
+            fifa.DisplayInfo();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ternary Challange/Ternary Challange/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ternary_Challange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature outside?");
+            string temperature = Console.ReadLine();
+            try
+            {
+                int tempInt = int.Parse(temperature);
+                string message = tempInt < 15 ? "It is too cold outside" : tempInt >= 16 && tempInt <= 28 ? "It is ok" : "It is hot here";
+                Console.WriteLine(message);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Oh dear, it seems you've not entered a number. Please try again");
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for using this program :)");
+            }
+            Console.Read();
+        }
+    }
+}
