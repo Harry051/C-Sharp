063373bc343f2913bcdedf1985e1b3c43df18148
Harry <109173178+Harry051@users.noreply.github.com>
2023-04-02 20:35:02 +0100

    Add files via upload

---
+++ XAML Intro/XAML Intro/MainWindow.xaml.vb
@@ -0,0 +1,37 @@
+﻿Class MainWindow
+    '   Seem to have loaded VB not C# but "code behind" would look like this:
+
+    'public MainWindow()
+    '{
+    '   IntiializeComponent();
+    '   Grid grid = new Grid();
+    '   this.Content = grid;
+    '   Button btn = new Button();
+    '   btn.Width = 100;
+    '   btn.Height = 150;
+    '   btn.FontSize = 28;
+
+    '   WrapPanel wrapPanel = new WrapPanel();
+    '   TextBlock txt = new TextBlock();
+    '   txt.Text = "Multi";
+    '   txt.Foreground = Brushes.Blue;
+    '   wrapPanel.Children.Add(txt);
+
+    '   WrapPanel wrapPanel = new WrapPanel();
+    '   TextBlock txt = new TextBlock();
+    '   txt.Text = "Colour";
+    '   txt.Foreground = Brushes.White;
+    '   wrapPanel.Children.Add(txt);
+
+    '   WrapPanel wrapPanel = new WrapPanel();
+    '   TextBlock txt = new TextBlock();
+    '   txt.Text = "Button";
+    '   txt.Foreground = Brushes.Red;
+    '   wrapPanel.Children.Add(txt);
+
+    '   btn.Content = wrapPanel;
+    '   grid.Children.Add(btn);
+
+    '
+
+End Class
