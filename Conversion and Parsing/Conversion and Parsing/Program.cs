2ce3019fcddf428f7bd558ecfc4b5ced32b6441c
Harry <109173178+Harry051@users.noreply.github.com>
2023-04-02 19:55:08 +0100

    Add files via upload

---
+++ Conversion and Parsing/Conversion and Parsing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Conversion_and_Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit conversioin
+            int num = 13784123;
+            long bigNum = num;
+
+            // Explicit conversion
+            double myDouble = 13.842;
+            int myInt;
+
+            myInt = (int)myDouble;  // This is called casting  myInt will be 13 as cant store decimal points.
+
+            // Type conversion
+            string myString = myDouble.ToString(); // Uses 'ToString' method to change the double value into a string.
+            Console.WriteLine(myString);
+
+            bool isItRaining = false;
+            string rainString = isItRaining.ToString();
+            Console.WriteLine(rainString);
+
+            // Parsing allows us to change strings into primitive data types
+            string favouriteNumber = "51";
+            int num1 = Int32.Parse(favouriteNumber);
+            double faveDouble = Double.Parse(favouriteNumber);
+            Console.WriteLine(num1);
+
+        }
+    }
+}
---
+++ Databinding/Databinding/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Databinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //If wanted to use OneTime(on initialization) data binding need to write some extra code
+
+            //MySlider.Value = 30;
+            //MyTextBox.Text = MySlider.Value.ToString();
+
+            //This code would set the value to 30 upon Start Up
+
+            
+        }
+
+        private void ColourTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            e.ToString();
+        }
+    }
+}
