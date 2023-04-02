d0904797cf8bbde89e748a6a019e751fb977c6d1
Harry <109173178+Harry051@users.noreply.github.com>
2023-04-02 20:13:55 +0100

    Add files via upload

---
+++ Net5App/Net5App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Net5App
+{
+    internal class Program
+    {
+        //Entry point of program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Harry!");
+            Console.Read();
+        }
+    }
+}
