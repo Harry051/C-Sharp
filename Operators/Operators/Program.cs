7f9a6afe00c8a7c0188559db518045df560c3e59
Harry <109173178+Harry051@users.noreply.github.com>
2023-04-02 20:15:08 +0100

    Add files via upload

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            // Unary operators
+            num3 = -num1;
+            Console.WriteLine($"num3 is {num3}");   // Just flips the number from 5 to -5
+
+            bool isSunny = true;
+            Console.WriteLine($"Is it sunny? {!isSunny}"); // The ! in front flips the value from true to false
+
+            // Increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine(num);
+            Console.WriteLine(num++);
+            Console.WriteLine(num);
+
+            // Preincrement operator
+            Console.WriteLine(++num); //<-- this will increment num on this line before it's called as opposed to the next line.
+
+            // Decrement operators
+            num--;
+            Console.WriteLine(num);         
+            Console.WriteLine(num--);
+            Console.WriteLine(num);
+
+            // Predecrement operators
+            Console.WriteLine(--num);
+
+            // Relational and Type operators
+            bool isLower;
+            isLower = num1 > num2;
+            Console.WriteLine(isLower);
+
+            // Equaltiy operators
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine(isEqual);
+
+            isEqual = num1 != num2;       // Will return true as != means NOT EQUAL
+            Console.WriteLine(isEqual);
+
+            // Conditional operators
+            bool isLowerAndSunny;
+            isLowerAndSunny = isLower && isSunny; //And statement. Will print true if both statements are true.
+            isLowerAndSunny = isLower || isSunny;//Or statement. Will print true if one statement is true.
+        }
+    }
+}
---
+++ params/params/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace paramLesson
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] myStory = { "Once", "upon", "a", "time", "I", "made", "an", "amazing", "array", "..." }; // This can go on forever
+        ParamsMethod(myStory);
+
+            int num = 51;
+            bool coolness = true;
+            string vibe = "mellow";
+            char at = '@';
+            ParamsMethod2(num, coolness, vibe, at);
+            ParamsMethod2(2, "Hello there Captain Ahab", 7.96892, false, true, 'P');
+
+
+            int whatIsSmall = findSmallestNumber(2, 384, 23, 987876757, 77, 1547, 3);
+            Console.WriteLine($"The smallest number from your list is: {whatIsSmall}");
+    }
+
+    public static void ParamsMethod(params string[] sentence)
+            //By using params keyword allows method to take a VARIABLE number of arguments from 0 to infinite
+            //In a nutshell lets the method be open and playful
+            //Important to note no additional parameters are permitted after the params keyword in the method decleration
+    {
+        for(int i = 0; i < sentence.Length; i++)
+        {
+            Console.WriteLine(sentence[i] + " ");
+        }
+    }
+
+        public static void ParamsMethod2(params object[] generalData)
+        {
+            foreach(object obj in generalData)
+            {
+                Console.WriteLine($"{obj} ");
+            }
+        }
+
+        //Real World example
+        public static int findSmallestNumber(params int[] numbers)
+        {
+            int smallest = int.MaxValue;
+
+            foreach(int number in numbers)
+            {
+                if(number < smallest)
+                {
+                    smallest = number;
+                }
+            }
+            return smallest;
+        }
+}
+}
