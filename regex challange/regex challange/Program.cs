0257760105105a4ad7d4d11d0d51d3e26b949435
Harry <109173178+Harry051@users.noreply.github.com>
2023-04-02 20:29:54 +0100

    Add files via upload

---
+++ regex challange/regex challange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace regex_challange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myText = System.IO.File.ReadAllText(@"C:\Users\harry\Downloads\input2.txt");
+            File.WriteAllText(@"C:\Users\harry\source\repos\regex challange\regex challange\ChallangeText.txt", myText);
+
+            string myRegex = @"\d{2,3}";
+            Regex regex = new Regex(myRegex);
+
+            MatchCollection myMC = regex.Matches(myText);
+
+            foreach (Match m in myMC)
+            {
+                //Console.WriteLine($"An example of your REGEX value: {m.Value} is found at index:{m.Index}");
+                Int32.TryParse(m.Value, out int mAsInt);
+                char mAsChar = (char)mAsInt;
+                Console.Write(mAsChar);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
